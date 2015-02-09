91cc89e0c982ed6c2cb348b44515d162cd72ab22
Sophal Kim <sophal.kim.work@gmail.com>
2015-02-09 19:39:02 +0000

    Remember function should work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  
+  attr_accessor :remember_token
   before_save { email.downcase! }
   
   validates :name, presence: true, length: { maximum: 50 }
@@ -16,5 +16,22 @@ class User < ActiveRecord::Base
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+  
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # Remembers a user in the database for use in persistent sessions.
+  def remember_token
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
   
 end
