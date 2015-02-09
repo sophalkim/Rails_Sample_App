bedc8248919bd42b7a6d4fb5917563b87821e95b
Sophal Kim <sophal.kim.work@gmail.com>
2015-02-09 17:57:09 +0000

    Valid inFormation test works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to user_path(user)
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
+  
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  # Returns the current logged-in user (if any).
+  # Returns nil, if there is no session[:user_id].
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+  
 end
